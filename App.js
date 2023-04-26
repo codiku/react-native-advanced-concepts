f7106c5cb9068ecf1c71e60941943fc4526d723f
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-26 11:53:14 +0200

    wip

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
 import { s } from "./App.style";
-import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
- 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <Text style={s.title}>Mes photos préférées</Text>
+        <Text style={s.title}>My favorite pictures</Text>
         <View style={s.body}></View>
         <View style={s.footer}>
-          <TouchableOpacity style={s.btn} onPress={()=>""}>
-            <Text style={s.btnTxt}>Ajouter photo</Text>
+          <TouchableOpacity style={s.btn} onPress={() => ""}>
+            <Text style={s.btnTxt}>Add picture</Text>
           </TouchableOpacity>
         </View>
       </SafeAreaView>
